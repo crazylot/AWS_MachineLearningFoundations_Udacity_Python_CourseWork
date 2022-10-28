0223c9a67edf20e8e4273b2b12d3f5c9aa040288
Muskan Kansal <muskan1810139@akgec.ac.in>
2022-10-28 17:48:52 +0530

    Pytest Working tested and added compute launch method

---
+++ DaysTolaunch.py
@@ -0,0 +1,2 @@
+def days_until_launch(current_day, launch_day):
+    return launch_day - current_day
--- UnitTesting/test_good.py
+++ UnitTesting/test_good.py
@@ -1,17 +1,17 @@
 from NearestPerfectSquare import nearest_perfect_square
 
 
-def nearest_n9():
+def test_nearest_n9():
     assert (nearest_perfect_square(-9) == 0)
 
 
-def nearest_0():
+def test_nearest_0():
     assert (nearest_perfect_square(0) == 0)
 
 
-def nearest_1():
+def test_nearest_1():
     assert (nearest_perfect_square(1) == 1)
 
 
-def nearest_12():
+def test_nearest_12():
     assert (nearest_perfect_square(12) == 9)
